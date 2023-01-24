4401cd9291dada7ed645337990a76180bfc5218d
DaniCis <danicisneros2@hotmail.com>
2023-01-24 00:09:33 -0500

    add color palette

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,10 @@
 import MainLayout from "../Layout/MainLayout";
 
 const HomePage = () => {
-
     return(
         <>
             <MainLayout>
-                hola
+                contenido tienda principal
             </MainLayout>
         </>
     )
