46b5dbb4a7e83e9806caa851af285d215f1b3b59
DaniCis <danicisneros2@hotmail.com>
2023-01-28 19:50:13 -0500

    services styles

--- src/components/home/MainMedServices.jsx
+++ src/components/home/MainMedServices.jsx
@@ -33,7 +33,7 @@ const MainMedServices = () =>{
         <div class="bg-white py-6 sm:py-8 lg:py-12">
             <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
                 <div class="mb-10 md:mb-16">
-                    <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6">Servicios Médicos</h2>
+                    <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6">Servicios <span className="text-primary-100">Médicos</span></h2>
                     <p class="max-w-screen-md text-gray-500 md:text-lg text-center mx-auto">This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated.</p>
                 </div>
                 <div 
