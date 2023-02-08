6e307f270d7fb4488614671e81991c2aac15709d
DaniCis <danicisneros2@hotmail.com>
2023-02-08 17:05:00 -0500

    add appointments

--- src/components/Appointment.jsx
+++ src/components/Appointment.jsx
@@ -1,14 +1,74 @@
+import { useFormik } from "formik";
+import { useState } from "react";
+import * as Yup from "yup";
+import SearchClient from "./SearchClient";
+
 const Appointment = () =>{
+    
+    const [openClient, setOpenClient] = useState(false)
+    const [doc, setDoc] = useState("")
+
+    const formik = useFormik({
+		initialValues: {
+            consultar:"",
+		},
+		validationSchema: Yup.object().shape({
+            consultar: Yup.number().typeError('Solo dígitos').min(10, 'Min 10 dígitos').required("Este campo es requerido"),
+        }),
+		onSubmit: (values) => {
+            setDoc(values.consultar)
+            setOpenClient(true)
+		},
+	})
+
     return(
         <>
             <input type="checkbox" id="cita" className="modal-toggle" />
             <div className="modal">
-                <div className="modal-box w-10/12 max-w-5xl">
-                    <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
-                    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                <div className="modal-box w-11/12 sm:w-10/12 max-w-5xl">
+                    <div className="text-center text-2xl font-semibold capitalize text-gray-600">
+                        <h2>Agendar nueva cita</h2>
+                    </div>
+                    <div className="grid grid-cols-1 md:grid-cols-2">
+                        <div className="mt-4">
+                            <p className="text-lg">Datos del Paciente</p>
+                            <form className="form-control mt-1" onSubmit={formik.handleSubmit}>
+                                <label className="label">
+                                    <span className="label-text text-gray-400">Buscar por Identificación</span>
+                                </label>
+                                <div className="input-group">
+                                    <input type="text" name="consultar" placeholder="Identificación" className="w-8/12 md:w-6/12 lg:w-8/12 px-4 py-3 rounded-md border border-gray-200 text-sm shadow-sm outline-none focus:z-10 focus:border-green-400 focus:ring-green-400 " onChange={formik.handleChange} value={formik.values.consultar} />
+                                    <button  onClick={formik.handleSubmit} type="submit" className="btn bg-primary-100">Consultar</button>
+                                </div>
+                                {formik.touched.consultar && formik.errors.consultar && (
+                                    <span className="text-red-400 flex text-xs">
+                                        {formik.errors.consultar}
+                                    </span>
+                                )}
+                            </form>
+                            {openClient &&(
+                                <SearchClient doc={doc} />
+                            )}
+                        </div>
+                        <div className="mt-4">
+                            <p className="text-lg font-medium text-gray-400">Seleccione fecha y hora para la cita</p>
+                            <div className="form-control w-full lg:w-6/12 mt-1">
+                                <label className="label">
+                                    <span className="label-text">Fecha</span>
+                                </label>
+                                <input type="date" name="fecha" className="px-4 py-3 rounded-md border border-gray-200 text-sm shadow-sm outline-none focus:z-10 focus:border-green-400 focus:ring-green-400"  />
+                            </div>
+                            <div className="form-control w-full lg:w-6/12 mt-1">
+                                <label className="label">
+                                    <span className="label-text">Hora</span>
+                                </label>
+                                <input type="time" name="hora" className="px-4 py-3 rounded-md border border-gray-200 text-sm shadow-sm outline-none focus:z-10 focus:border-green-400 focus:ring-green-400"  />
+                            </div>
+                            <button className="mt-4 bg-green-500 rounded-md px-4 py-2 text-white cursor-pointer hover:bg-green-700">Agendar</button>
+                        </div>
+                    </div>
                     <div className="modal-action">
-                        <label htmlFor="cita" className="btn bg-error-100">Cancelar</label>
-                        <label htmlFor="cita" className="btn bg-green-600">Agendar</label>
+                        <label htmlFor="cita" className="btn btn-sm bg-error-100 border-none">Cerrar</label>
                     </div>
                 </div>
             </div>
---
+++ src/components/SearchClient.jsx
@@ -0,0 +1,112 @@
+import { useAddNewClientMutation, useGetClientByDocumentQuery } from "../store/serverApi";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const SearchClient =({doc})=>{
+
+    const {data: cliente, isError, isSuccess} = useGetClientByDocumentQuery(doc)
+    const [addNewPost] = useAddNewClientMutation()
+
+    const appSchema = Yup.object().shape({
+		nombre: Yup.string().required("Este campo es requerido"),
+        apellido: Yup.string().required("Este campo es requerido"),
+        ident: Yup.number().typeError('Solo dígitos').min(10, 'Min 10 dígitos').required("Este campo es requerido"),
+        telf: Yup.number().typeError('Solo dígitos').required("Este campo es requerido")
+	});
+
+    const formik = useFormik({
+		initialValues: {
+			nombre:"",
+            apellido:"",
+            ident:doc,
+            telf:""
+		},
+		validationSchema: appSchema,
+		onSubmit: (values) => {
+            addNewPost({ 
+                document: values.ident,
+                firstName: values.nombre,
+                lastName: values.apellido,
+                phone: values.telf
+            })
+		},
+	});
+
+
+    return(
+        <>
+            {isSuccess && (
+                <div className="border-1 p-4">
+                    <p className="text-gray-600">Nombre: <span className="text-gray-400">{cliente.firstName}</span></p>
+                    <p className="text-gray-600">Apellido: <span className="text-gray-400">{cliente.lastName}</span></p>
+                    <p className="text-gray-600">Teléfono: <span className="text-gray-400">{cliente.phone}</span></p>
+                </div>
+            )}
+            {isError && (
+                <>
+                    <p className="mt-2 text-gray-400 text-sm">Paciente no encontrado</p>
+                    <form onSubmit={formik.handleSubmit}>
+                        <div className='grid grid-cols-1 md:grid-cols-2'>
+                            <div className="form-control w-full md:w-10/12 max-w-sm mt-1">
+                                <label className="label">
+                                    <span className="label-text">Nombre</span>
+                                </label>
+                                <input type="text" name="nombre" placeholder="Nombre" className="px-4 py-3 rounded-md border border-gray-200 text-sm shadow-sm outline-none focus:z-10 focus:border-green-400 focus:ring-green-400" onChange={formik.handleChange} value={formik.values.nombre} />
+                                {formik.touched.nombre && formik.errors.nombre && (
+                                    <span className="text-red-400 flex text-xs">
+                                        {formik.errors.nombre}
+                                    </span>
+                                )}
+                            </div>
+                            <div className="form-control w-full md:w-10/12 max-w-sm mt-1">
+                                <label className="label">
+                                    <span className="label-text">Apellido</span>
+                                </label>
+                                <input type="text" name="apellido" placeholder="Apellido" className="px-4 py-3 rounded-md border border-gray-200 text-sm shadow-sm outline-none focus:z-10 focus:border-green-400 focus:ring-green-400" onChange={formik.handleChange} value={formik.values.apellido} />
+                                {formik.touched.apellido && formik.errors.apellido && (
+                                    <span className="text-red-400 flex text-xs">
+                                        {formik.errors.apellido}
+                                    </span>
+                                )}
+                            </div>
+                        </div>
+                        <div className='grid grid-cols-1 md:grid-cols-2'>
+                            <div className="form-control w-full md:w-10/12 max-w-sm mt-1">
+                                <label className="label">
+                                    <span className="label-text">Identificación</span>
+                                </label>
+                                <input type="text" name="ident" placeholder="Identificación" className="px-4 py-3 rounded-md border border-gray-200 text-sm shadow-sm outline-none focus:z-10 focus:border-green-400 focus:ring-green-400 " onChange={formik.handleChange} value={formik.values.ident} />
+                                {formik.touched.ident && formik.errors.ident && (
+                                    <span className="text-red-400 flex text-xs">
+                                        {formik.errors.ident}
+                                    </span>
+                                )}
+                            </div>
+                            <div className="form-control w-full md:w-10/12 max-w-sm mt-1">
+                                <label className="label">
+                                    <span className="label-text">Teléfono</span>
+                                </label>
+                                <input type="text" name="telf" placeholder="Teléfono" className="px-4 py-3 rounded-md border border-gray-200 text-sm shadow-sm outline-none focus:z-10 focus:border-green-400 focus:ring-green-400 " onChange={formik.handleChange} value={formik.values.telf} />
+                                {formik.touched.telf && formik.errors.telf && (
+                                    <span className="text-red-400 flex text-xs">
+                                        {formik.errors.telf}
+                                    </span>
+                                )}
+                            </div>
+                        </div>
+                        {cliente!= null && (
+                            <button onClick={formik.handleSubmit} type="submit" className="mt-6 mb-8 w-full sm:w-11/12 rounded-md bg-primary-80 px-6 py-3 font-medium text-white">
+                            Registrar
+                            </button>
+                        )}
+                    </form>
+                </>
+               
+            )}
+        </>
+        
+    )
+
+}
+
+export default SearchClient;
